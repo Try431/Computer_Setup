6d6165be77f9686be1bd474515afa930506f786c
Isaac Garza <garzai@alum.mit.edu>
2020-05-07 16:54:12 -0700

    Updating scripts

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,34 +2,24 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# echo "THIS SHOULD BE HERE TWIIIIIIIICE"
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-# 	# echo "WHAAAAAAAT"
-#     MONITOR=$m
-#     polybar -l info --reload example &
-#   done
-# else
-#   polybar -l info example &
-# fi
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    export MONITOR=$m 
+    echo $MONITOR >> ~/test.txt
+    polybar --reload primary &
+  done
+else
+  polybar --reload primary &
+fi
 
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload primary &
-#   done
-# else
-#   polybar --reload primary &
-# fi
 
-# Launch bar1 and bar2
-# MONITOR=eDP-1-1 polybar blank &
-MONITOR=eDP-1-1 polybar primary 
-# MONITOR=HDMI-0 polybar --reload example
-# polybar primary &
-
+# export MONITOR=eDP-1-1
+# polybar --reload primary &
+# export MONITOR=HDMI-0
+# polybar --reload primary &
 
 echo "Bars launched..."
